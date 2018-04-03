717127f47909d385abefdef316e37fa86fe89cda
Bond <bond-d@ukr.net>
2018-04-03 11:57:03 +0300

    PC FAIL CHANCE COMMIT

--- src/Fixer.java
+++ src/Fixer.java
@@ -3,13 +3,22 @@ package src;
 import process.Actor;
 import process.DispatcherFinishException;
 import process.QueueForTransactions;
+import widgets.ChooseRandom;
 
 public class Fixer extends Actor {
 	private QueueForTransactions<PC> queue;
 	private QueueForTransactions<PC> f_queue;
 	private QueueForTransactions<PC> p_queue;
+	private double finishTime;
+	private ChooseRandom rnd;
 	public Fixer(String string, GUI gui, Model model) {
 		// TODO Auto-generated constructor stub
+		//setNameForProtocol(name);
+		finishTime = gui.getChooseData_Modelling_Time().getDouble();
+		rnd = gui.getChooseRandom_Fixing_Time();
+		queue = model.getQueue();
+		f_queue = model.getFQueue();
+		p_queue = model.getPQueue();
 	}
 	
 	@Override
